ba8600ed59703bd17fe97b531a350ad92a623b4c
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-12-13 15:24:52 +0100

    l'insertion sur la table de reservation .

--- page.php
+++ page.php
@@ -22,6 +22,18 @@ if($forma_query){
     echo "Erreur !";
 }
 
+$requete_reserv="INSERT INTO reservation(ID_Membre,ID_Activite,Date_Reservation) values ('$last_id','$ana',CURRENT_DATE)";
+$query_reservation = mysqli_query($conn,$requete_reserv);
 
+if($query_reservation){
+    // $last_id = mysqli_insert_id($conn);
+    // echo $last_id;
+    // echo"<h1>insertion Res avec success</h1>";
+    header("location: affichage.php");
+    // exit();
+}
+else{
+    echo"<h1>erreur Res d'insertion</h1>";
+}
 
 ?>
