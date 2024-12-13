3ca0ca508a905c696659456203cfd4437f16373c
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-12-13 15:29:24 +0100

    modification sur db.

--- database.php
+++ database.php
@@ -1,1 +1,19 @@
+<?php
 
+// $db_server = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "gestion_salle_sport";
+// $conn = "";
+
+
+
+$conn = new mysqli("localhost", "root", "", "gestion_salle_sport");
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Erreur de connexion :  !!!!!!!!!!!!!!!!" . $conn->connect_error);
+}
+else {
+    echo"you are connected !!!";}
+    
