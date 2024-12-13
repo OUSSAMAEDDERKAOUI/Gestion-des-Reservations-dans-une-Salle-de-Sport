e218d0426eafa783c7bd3f96f1bf28b10e252df5
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-12-13 15:21:09 +0100

    L'ajoute de page home .

--- database.php
+++ database.php
@@ -1,19 +1,1 @@
-<?php
 
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "gestion_salle_sport";
-$conn = "";
-try{
-    $conn = new PDO ("mysql:host=$db_server;dbname=$db_name",$db_user,$db_pass) ;
-echo"you are connected !!!";
-}
-catch(PDOException $e ){
-     die('Erreur:  '.$e->getMessage());
-
-}
-
-
-
-?>
