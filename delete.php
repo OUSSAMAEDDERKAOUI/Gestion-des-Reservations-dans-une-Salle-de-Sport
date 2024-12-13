50a0acd58e14592acfbd6cbf093239af77be4996
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-12-13 15:32:07 +0100

    l'ajoute de page delete .

--- delete.php
+++ delete.php
@@ -1,3 +1,11 @@
 
-
+<?php
+require 'database.php' ;
+$id_deleted=$_GET['id'];
+ $requete= "DELETE FROM Reservation WHERE ID_Resrvation = '$id_deleted' ";
+$query = mysqli_query($conn,$requete); 
+if($query){
+    header("location:affichage.php");
+}
+?>
 
